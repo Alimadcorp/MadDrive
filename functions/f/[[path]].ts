83a1e66af536d6a686a7f5ba070c5dc9138c8ff3
Alimadcorp <119115454+Alimadcorp@users.noreply.github.com>
2025-11-17 01:38:34 +0500

    SOem changes

--- functions/f/[[path]].ts
+++ functions/f/[[path]].ts
@@ -12,7 +12,12 @@ import { handleRequestPost } from "./post";
 
 async function handleRequestOptions() {
   return new Response(null, {
-    headers: { Allow: Object.keys(HANDLERS).join(", ") },
+    headers: {
+      Allow: Object.keys(HANDLERS).join(", "),
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "HEAD, OPTIONS",
+      "Access-Control-Allow-Headers": "*",
+    },
   });
 }
 
